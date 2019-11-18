3892072c302ed00be334ca75e4daf4ce3907b4d7
George Beckstein <becksteing@embeddedplanet.com>
2019-11-18 15:33:10 -0500

    Implemented assert function for matching flags and bitstring

--- UNITTESTS/platform/CallChain/test_CallChain.cpp
+++ UNITTESTS/platform/CallChain/test_CallChain.cpp
@@ -17,27 +17,65 @@
 
 #include "gtest/gtest.h"
 
+#include "extensions/CallChain.h"
+
+#include <algorithm>
+#include <string>
 
 /*
  *
  */
 
+#define MAX_NUM_BITFLAGS 32
+
 class TestCallChain : public testing::Test {
     
-	
 	virtual void SetUp()
+	
     {
-    	
+		//memset()
     }
 
     virtual void TearDown()
     {
     }
 
+public:
+
+
+    /**
+     * Ensures the flags set by each callback match the expected bitstring
+     * This function asserts failure if not.
+     *
+     * @param[in] bitstring C-style string of '1'/'0' characters representing
+     * the expected bit flags to be set by the callbacks in a test.
+     *
+     * IE: callback_1 added, callback_2 added, callback 1 removed ->
+     * expected test result is "10"
+     *
+     *
+     * @note this is not thread safe
+     */
+    void assert_matching_flags(const std::string bitstring) {
+
+    	/** Compare each character in the string to its corresponding flag boolean */
+
+    	// Reset the position in the bit flags
+    	current_bit = bit_flags;
+    	std::for_each(bitstring.begin(), bitstring.end(),[this](const char& c) {
+    		bool expected = ((c == '1')? true : false);
+    		EXPECT_EQ(*current_bit, expected);
+    	});
+    }
+
+    bool* current_bit; /** Current bit being checked */
+    bool bit_flags[MAX_NUM_BITFLAGS];
+
+private:
+
 };
 
 TEST_F(TestCallChain, constructor)
 {
-
 
 }
