800577868d2def4f575c32d4062730754456a973
George Beckstein <becksteing@embeddedplanet.com>
2019-11-25 11:04:45 -0500

    Updated file headers to be consistent.

--- drivers/DigitalButton.h
+++ drivers/DigitalButton.h
@@ -1,10 +1,26 @@
 /**
- * @file DigitalButton.h
- * @brief A button input child class based on the DebounceIn class
+ * ep-oc-mcu
+ * Embedded Planet Open Core for Microcontrollers
  *
- *  Created on: May 6, 2019
- *      Author: gdbeckstein
+ * Built with ARM Mbed-OS
+ *
+ * Copyright (c) 2019 Embedded Planet, Inc.
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
+
 #ifndef DRIVERS_DIGITALBUTTON_H_
 #define DRIVERS_DIGITALBUTTON_H_
 
