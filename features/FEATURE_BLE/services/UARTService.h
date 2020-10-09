b25d192f1501097dbfa57552cd20e5132b5799e1
George Beckstein <donbeckstein@live.com>
2020-10-09 12:58:28 -0400

    Reimplemented UARTService to have connection-based API
        The previous implementation did not handle the use case where there were multiple clients connected to the same GattServer. The new API has been designed to be connection-based, where each new connection handle is allocated its own BleSerial. The BleSerial class acts as a FileHandle and can thus be used as a debug output or like any other serial class (BufferedUART, etc).

--- features/FEATURE_BLE/services/UARTService.h
+++ features/FEATURE_BLE/services/UARTService.h
@@ -19,6 +19,9 @@
 
 #include "platform/FileHandle.h"
 #include "platform/PlatformMutex.h"
+#include "platform/CircularBuffer.h"
+#include "platform/NonCopyable.h"
+#include "platform/Span.h"
 
 #include "ble/common/UUID.h"
 #include "ble/BLE.h"
@@ -28,6 +31,8 @@
 #include "rtos/Queue.h"
 
 #include <string>
+
+#include <forward_list>
 
 #if BLE_FEATURE_GATT_SERVER
 
@@ -57,10 +62,16 @@ extern const uint8_t  UARTServiceRXCharacteristicUUID[UUID::LENGTH_OF_LONG_UUID]
 #endif
 
 /**
- * TX and RX Queues
+ * BLE defaults to 23 bytes for MTU size, subtract 3 for overhead and you get 20 bytes
+ * per connection interval by default
+ */
+#define BLE_UART_SERVICE_DEFAULT_MTU_SIZE 20
+
+/**
+ * TX and RX Buffer Sizes
  */
 #ifndef MBED_CONF_BLE_UART_SERVICE_TX_RX_QUEUE_SIZE
-#define MBED_CONF_BLE_UART_SERVICE_TX_RX_QUEUE_SIZE 32
+#define MBED_CONF_BLE_UART_SERVICE_TX_RX_BUFFER_SIZE 256
 #endif
 
 /**
@@ -68,51 +79,169 @@ extern const uint8_t  UARTServiceRXCharacteristicUUID[UUID::LENGTH_OF_LONG_UUID]
  * @brief BLE Service to enable UART over BLE.
  *
  */
-class UARTService : public mbed::FileHandle,
-                    public ble::GattServer::EventHandler,
-                    public ble::Gap::EventHandler {
+class UARTService : public ble::GattServer::EventHandler,
+                    public ble::Gap::EventHandler,
+                    private mbed::NonCopyable<UARTService> {
 
 public:
 
     /**
-     * String with associated connection handle
+     * Serial representing a transfer for a single
+     * BLE connection handle.
      */
-    class TransceivedString
-    {
+    class BleSerial :
+            public mbed::FileHandle,
+            private mbed::NonCopyable<BleSerial> {
+
+    /** Allow UARTService to construct this class */
+    friend UARTService;
+
+    protected:
+
+        BleSerial(UARTService& service,
+                ble::connection_handle_t connection_handle);
+
+        ~BleSerial();
+
     public:
 
-        TransceivedString(const char* s, size_t n, ble::connection_handle_t connection_handle = 0) : _str(s, n),
-                _connection_handle(connection_handle), _is_global(false) { }
+        ssize_t write(const void *_buffer, size_t length) override;
+
+        ssize_t read(void *buffer, size_t size) override;
+
+        off_t seek(off_t offset, int whence = SEEK_SET) override {
+            /* Seeking is not support by this file handler */
+            return -ESPIPE;
+        }
+
+        off_t size() override {
+            /* Size is not defined for this file handle */
+            return -EINVAL;
+        }
+
+        int isatty() override {
+            /* File handle is used for terminal output */
+            return true;
+        }
+
+        int close() override {
+            return 0;
+        }
+
+        int set_blocking(bool blocking) override {
+            _blocking = blocking;
+            return 0;
+        }
+
+        bool is_blocking() const override {
+            return _blocking;
+        }
+
+        void sigio(mbed::Callback<void()> func) override {
+            _sigio_cb = func;
+        }
+
+        ble::connection_handle_t get_connection_handle(void) {
+            return _connection_handle;
+        }
+
+        bool operator==(const BleSerial& other) {
+            return (_connection_handle == other._connection_handle);
+        }
 
         /**
-         * For TX Strings, if _is_global is true, the packet is sent
-         * to all connection handles and connection_handle is ignored
+         * Set the application callback for when updates (notifications/indications)
+         * are enabled/disabled.
          */
-        void set_global(bool global) {
-            _is_global = global;
+        void on_updates_changed(mbed::Callback<void(bool)> updates_changed_cb) {
+            _updates_changed_cb = updates_changed_cb;
         }
 
-        bool is_global(void) const {
-            return _is_global;
-        }
-
-        const std::string get_string(void) const {
-            return _str;
-        }
-
-        ble::connection_handle_t get_connection_handle(void) const {
-            return _connection_handle;
+        uint16_t get_mtu(void) {
+            return _mtu;
         }
 
     protected:
 
-        std::string _str;
-        ble::connection_handle_t _connection_handle;
-        bool _is_global;
+        /** Handler for when updates are enabled by this connection */
+        void on_updates_enabled(void);
+
+        /** Handler for when updates are disabled by this connection */
+        void on_updates_disabled(void);
+
+        /**
+         * Handler for when data has been sent to a client as part
+         * of a notification/indication
+         */
+        void on_data_sent(void);
+
+        /**
+         * Handler for when data has been written to the RX characteristic
+         * by the associated connection handle
+         */
+        void on_data_written(mbed::Span<const uint8_t> data);
+
+        /**
+         * Deallocates the tx buffer, called upon both disconnection
+         * and if the client disables updates
+         */
+        void deallocate_tx_buffer(void) {
+            delete _txbuf;
+            _txbuf = nullptr;
+        }
+
+        void wake(void) {
+            if(_sigio_cb) {
+                _sigio_cb();
+            }
+        }
+
+        void set_mtu(uint16_t mtu) {
+            _mtu = mtu;
+        }
+
+
+    protected:
+
+        UARTService& _service;
+        ble::connection_handle_t _connection_handle = 0;
+        bool _blocking = true;
+
+        mbed::CircularBuffer<uint8_t, MBED_CONF_BLE_UART_SERVICE_TX_RX_BUFFER_SIZE> _rxbuf;
+
+        /** The TX buffer is only allocated if the client subscribes to the characteristic */
+        mbed::CircularBuffer<uint8_t, MBED_CONF_BLE_UART_SERVICE_TX_RX_BUFFER_SIZE>* _txbuf = nullptr;
+
+        mbed::Callback<void(void)> _sigio_cb = nullptr;
+
+        /**
+         * Application callback for when updates are enabled/disabled on
+         * this serial connection handle
+         *
+         * @param[in] enabled True if updates have been enabled, false if disabled
+         */
+        mbed::Callback<void(bool)> _updates_changed_cb = nullptr;
+
+        PlatformMutex mutex;
+
+        /** Cached MTU of the connection */
+        uint16_t _mtu;
+
+        /** Gatt TX buffer */
+        uint8_t *_gatt_tx_buf = nullptr;
+
+        /** Actively writing from tx buffer flag */
+        bool _sending_data = false;
 
     };
 
 public:
+
+    /**
+     * Allow BleSerial to access write
+     * TODO - is friendship of an internal class intrinsic in C++11?
+     */
+    friend BleSerial;
 
     /**
     * @param _ble
@@ -120,168 +249,54 @@ public:
     */
     UARTService();
 
-    virtual ~UARTService() { }
+    virtual ~UARTService();
 
     void start(BLE &ble_interface);
 
-    /**
-     * We attempt to collect bytes before pushing them to the UART RX
-     * characteristic; writing to the RX characteristic then generates
-     * notifications for the client. Updates made in quick succession to a
-     * notification-generating characteristic result in data being buffered
-     * in the Bluetooth stack as notifications are sent out. The stack has
-     * its limits for this buffering - typically a small number under 10.
-     * Collecting data into the sendBuffer buffer helps mitigate the rate of
-     * updates. But we shouldn't buffer a large amount of data before updating
-     * the characteristic, otherwise the client needs to turn around and make
-     * a long read request; this is because notifications include only the first
-     * 20 bytes of the updated data.
-     *
-     * @param  _buffer The received update.
-     * @param  length Number of characters to be appended.
-     * @return        Number of characters appended to the rxCharacteristic.
-     */
-    ssize_t write(const void *_buffer, size_t length) override;
+    /** GattServer::EventHandler overrides */
+    virtual void onAttMtuChange(
+            ble::connection_handle_t connectionHandle,
+            uint16_t attMtuSize) override;
 
-    ssize_t read(void *buffer, size_t size) override;
-
-    off_t seek(off_t offset, int whence = SEEK_SET) override
-    {
-        /* Seeking is not support by this file handler */
-        return -ESPIPE;
-    }
-
-    off_t size() override
-    {
-        /* Size is not defined for this file handle */
-        return -EINVAL;
-    }
-
-    int isatty() override
-    {
-        /* File handle is used for terminal output */
-        return true;
-    }
-
-    int close() override
-    {
-        return 0;
-    }
-
-    /** Set blocking or non-blocking mode
-     *  The default is blocking.
-     *
-     *  @param blocking true for blocking mode, false for non-blocking mode.
-     */
-    int set_blocking(bool blocking) override
-    {
-        _blocking = blocking;
-        return 0;
-    }
-
-    /** Check current blocking or non-blocking mode for file operations.
-     *
-     *  @return true for blocking mode, false for non-blocking mode.
-     */
-    bool is_blocking() const override
-    {
-        return _blocking;
-    }
-
-    void sigio(mbed::Callback<void()> func) override;
-
-    /**
-     * Set the application callback for when updates (notifications/indications)
-     * are enabled/disabled.
-     */
-    void on_updates_changed(mbed::Callback<void(ble::connection_handle_t, bool)> updates_changed_cb) {
-        _updates_changed_cb = updates_changed_cb;
-    }
-
-    /**
-     * Function invoked when the server has sent data to a client as
-     * part of a notification/indication.
-     *
-     * @note params has a temporary scope and should be copied by the
-     * application if needed later
-     */
     void onDataSent(const GattDataSentCallbackParams &params) override;
 
-    /**
-     * Function invoked when a client writes an attribute
-     *
-     * @note params has a temporary scope and should be copied by the
-     * application if needed later
-     */
     void onDataWritten(const GattWriteCallbackParams &params) override;
 
-    /**
-     * Function invoked when the GattServer instance is about
-     * to be shut down. This can result in a call to reset() or BLE::reset().
-     */
-    void onShutdown(const GattServer &server) override {
-        // TODO - flush? anything else before shutdown?
-        (void)server;
-    }
+    void onShutdown(const GattServer &server) override;
 
-    /**
-     * Function invoked when the client has subscribed to characteristic updates
-     *
-     * @note params has a temporary scope and should be copied by the
-     * application if needed later
-     */
     void onUpdatesEnabled(const GattUpdatesEnabledCallbackParams &params) override;
 
-    /**
-     * Function invoked when the client has unsubscribed to characteristic updates
-     *
-     * @note params has a temporary scope and should be copied by the
-     * application if needed later
-     */
     void onUpdatesDisabled(const GattUpdatesDisabledCallbackParams &params) override;
 
-    /**
-     * Called when connection attempt ends or an advertising device has been
-     * connected.
-     *
-     * @see startAdvertising()
-     * @see connect()
-     *
-     * @param event Connection event.
-     */
+    /** Gap::EventHandler overrides */
+
     void onConnectionComplete(const ble::ConnectionCompleteEvent &event) override;
 
-    /**
-     * Called when a connection has been disconnected.
-     *
-     * @param event Details of the event.
-     *
-     * @see disconnect()
-     */
     void onDisconnectionComplete(const ble::DisconnectionCompleteEvent &event) override;
 
-protected:
-
     /**
-     * Internal write processing function
+     * Gets a BleSerial pointer given a connection handle.
      *
-     * @retval True if txQueue is empty
+     * @param[in] connection_handle Connection handle to get the associated BleSerial handle of
+     * @retval Pointer to associated BleSerial object, nullptr if not found
      */
-    bool _write(void);
+    BleSerial* get_ble_serial_handle(ble::connection_handle_t connection_handle);
 
 protected:
 
-    uint8_t connection_count;   /** Counter for number of current connections */
-    uint8_t send_countdown;     /** Countdown for onDataSent events */
-    bool sending;               /** True if currently sending notifications to connected peers */
+    void delete_all_serial_handles(void);
 
-    uint8_t             receiveBuffer[BLE_UART_SERVICE_MAX_DATA_LEN]; /**< The local buffer into which we receive
-                                                                       *   inbound data before forwarding it to the
-                                                                       *   application. */
+    ble_error_t write(BleSerial* ser, mbed::Span<const uint8_t> data);
 
-    uint8_t             sendBuffer[BLE_UART_SERVICE_MAX_DATA_LEN];    /**< The local buffer into which outbound data is
-                                                                       *   accumulated before being pushed to the
-                                                                       *   rxCharacteristic. */
+protected:
+
+    uint8_t receiveBuffer[BLE_UART_SERVICE_MAX_DATA_LEN]; /**< The local buffer into which we receive
+                                                           *   inbound data before forwarding it to the
+                                                           *   application. */
+
+    uint8_t sendBuffer[BLE_UART_SERVICE_MAX_DATA_LEN];    /**< The local buffer into which outbound data is
+                                                           *   accumulated before being pushed to the
+                                                           *   rxCharacteristic. */
 
     GattCharacteristic  txCharacteristic; /**< From the point of view of the external client, this is the characteristic
                                            *   they'd write into in order to communicate with this application. */
@@ -297,23 +312,8 @@ protected:
 
     GattServer* _server;
 
-    /**
-     * Application callback for when updates are enabled/disabled
-     * Called with true as argument when updates are enabled
-     * Called with false as argument when updates are disabled
-     */
-    mbed::Callback<void(ble::connection_handle_t, bool)> _updates_changed_cb = nullptr;
-
-    rtos::Queue<TransceivedString, MBED_CONF_BLE_UART_SERVICE_TX_RX_QUEUE_SIZE> rxQueue; /** This is from our point of view (rxQueue = messages waiting to be read) */
-    rtos::Queue<TransceivedString, MBED_CONF_BLE_UART_SERVICE_TX_RX_QUEUE_SIZE> txQueue; /** This is from our point of view (txQueue = messages waiting to be sent) */
-
-    mbed::Callback<void()> _sigio_cb = nullptr;
-
-    TransceivedString* next = nullptr;
-
-    bool _blocking = true;
-
-    PlatformMutex mutex;
+    /** Linked list of serial handles for each connection */
+    std::forward_list<BleSerial*> _serial_handles;
 
 };
 
